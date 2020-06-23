0dea973bf6ba5f47f26fd875f8064c0451fdac85
gm_desktop <gmesda23@gmail.com>
2020-06-23 16:20:09 -0400

    fixed typos

--- src/components/body.js
+++ src/components/body.js
@@ -9,7 +9,7 @@ function Body() {
     return (
         <div className="main-container">
             {itemList}
-      </div>
+        </div>
     )
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,8 @@ import React from 'react';
 function Header() {
     return (
         <div className="header-container">
-            <h1> What are your main aread of focus?</h1>
-            <h4> This will help us build a personalize experience for you </h4>
+            <h1> What are your main areas of focus?</h1>
+            <h4> This will help us build a personalized experience for you </h4>
         </div>
     )
 }
